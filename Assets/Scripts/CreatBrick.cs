5504af9ea0d3516605ab711bdabe127c372d6683
Einstephener <kihyuk0201@gmail.com>
2023-11-30 20:15:44 +0900

    chore - 브릭 빈칸없이 생성
    이제 더 이상 빈칸이 생기지 않음.

--- Assets/Scripts/CreatBrick.cs
+++ Assets/Scripts/CreatBrick.cs
@@ -6,37 +6,39 @@ public class CreatBrick : MonoBehaviour
 {
     int brickStocks = 0;
     int difficult = 1;
-    int bricksLeft = 0;
     public GameObject brick;
     public GameObject bricks;
     bool[] check = new bool[0];
     void Start()
     {
         brickStocks = 7 * (3 + difficult);
-        //자리가 남아있는지 확인하는 변수
-        bricksLeft = brickStocks;
         check = new bool[brickStocks];
         for (int i = 0; i < brickStocks; i++)
         {
             check[i] = false;
         }
         Time.timeScale = 1.0f;
-        for (int i = 0; i < (brickStocks - 1); i++)
+        for (int i = 0; i < brickStocks; i++)
         {
             GenerateBrick(i);
         }
     }
     void GenerateBrick(int i)
     {
-        int rand = Random.Range(0, brickStocks);
-        if (check[rand] == false)
+        int rand;
+        do
         {
-            check[rand] = true;
-            GameObject newCard = Instantiate(brick);
-            newCard.transform.parent = GameObject.Find("Bricks").transform;
-            float x = (rand % 7) * 3f - 9f;
-            float y = (rand / 7) * 2f + 3f;
-            newCard.transform.position = new Vector3(x, y, 0);
-        }
+            rand = Random.Range(0, brickStocks);
+        } while (check[rand]);
+
+        check[rand] = true;
+
+        GameObject newCard = Instantiate(brick);
+        newCard.transform.parent = GameObject.Find("Bricks").transform;
+
+        float x = (rand % 7) * 3f - 9f;
+        float y = (rand / 7) * 2f + 3f;
+
+        newCard.transform.position = new Vector3(x, y, 0);
     }
 }
