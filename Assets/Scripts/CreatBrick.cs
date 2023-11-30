7efab5bfbf44b187c9fe91e80d98c71746860312
Einstephener <kihyuk0201@gmail.com>
2023-11-30 19:20:46 +0900

    feat- 랜덤 위치에 브릭 생성
    랜덤한 위치에 브릭 생성
    하지만 브릭이 부서지지 않음-> 수정필요

---
+++ Assets/Scripts/CreatBrick.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatBrick : MonoBehaviour
+{
+    //int cardStocks = 0;
+    //int difficult = 1;
+    //int cardsLeft = 0;
+    //public GameObject brick;
+    //public GameObject bricks;
+    //bool[] check = new bool[0];
+    //void Start()
+    //{
+    //    cardStocks = 7 * (3 + difficult);
+    //    자리가 남아있는지 확인하는 변수
+    //    cardsLeft = cardStocks;
+    //    check = new bool[cardStocks];
+    //    for (int i = 0; i < cardStocks; i++)
+    //    {
+    //        check[i] = false;
+    //    }
+    //    Time.timeScale = 1.0f;
+    //    for (int i = 0; i < (cardStocks - 1); i++)
+    //    {
+    //        GenerateBrick(i);
+    //    }
+    //}
+    //void GenerateBrick(int i)
+    //{
+    //    int rand = Random.Range(0, cardStocks);
+    //    if (check[rand] == false)
+    //    {
+    //        check[rand] = true;
+    //        GameObject newCard = Instantiate(brick);
+    //        newCard.transform.parent = GameObject.Find("Bricks").transform;
+    //        float x = (rand % 4) * 1.4f - 2.1f;
+    //        float y = -(rand / 4) * 1.4f + (0.8f + 0.4f * difficult);
+    //        newCard.transform.position = new Vector3(x, y, 0);
+    //    }
+    //}
+
+    //Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+}
