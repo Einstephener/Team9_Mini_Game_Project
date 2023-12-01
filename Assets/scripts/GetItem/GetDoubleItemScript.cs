b8652617547fa3257a278d282c65efc962549d68
hyeon-tak <149382994+hyeon-tak@users.noreply.github.com>
2023-12-01 12:05:24 +0900

    feat - 투명도 조절 아이템
    현재 버그있음 IsTransparent가 true가 되면 Update()가 멈춤(확인중)

--- Assets/scripts/GetItem/GetDoubleItemScript.cs
+++ Assets/scripts/GetItem/GetDoubleItemScript.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class GetDoubleItemScript : MonoBehaviour
 {
-    public GameObject Ball;
     public int numberOfBallsToAdd = 1;
     public float newBallSpeed = 5f;
 
@@ -17,15 +16,7 @@ public class GetDoubleItemScript : MonoBehaviour
             // DoubleItem을 통해 늘어날 공의 갯수만큼 반복
             for (int i = 0; i < numberOfBallsToAdd; i++)
             {
-
-                // 현재 위치에서 새로운 공 생성
-                //GameObject newBall = Instantiate(Ball, paddlePosition, Quaternion.identity);
                 GameManager.I.BallAdd();
-                // 현재 패들 위치에서 새로운 공 생성
-                //GameObject newBall = Instantiate(Ball, paddlePosition, Quaternion.identity);
-
-                // 생성된 공에 대한 초기화 작업 수행
-                //newBall.GetComponent<Rigidbody2D>().velocity = Vector2.up * newBallSpeed;
             }
 
             // 현재의 DoubleItem 오브젝트를 파괴
--- Assets/scripts/GetItem/GetSpeedItemScript.cs
+++ Assets/scripts/GetItem/GetSpeedItemScript.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class GetSpeedItemScript : MonoBehaviour
 {
-    public float ballSpeedMultiplier = 1.5f;
-
     private void OnTriggerEnter2D(Collider2D other)
     {   //부딪힌 오브젝트의 태그가 Paddle인지 확인
         if (other.CompareTag("Paddle"))
