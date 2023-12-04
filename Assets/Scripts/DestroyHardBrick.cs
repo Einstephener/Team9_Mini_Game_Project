e523f24f39290ecad3d8c392ec5a81a80a5783c1
Einstephener <kihyuk0201@gmail.com>
2023-12-04 11:48:57 +0900

    feat - 새 스테이지, 강화된 벽돌
    새로운 스테이지에 강화된 벽돌이 나오도록 설정(강화된 벽돌 = 2번 충돌시 파괴)

---
+++ Assets/Scripts/DestroyHardBrick.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyHardBrick : MonoBehaviour
+{
+    int count = 0;
+    SpriteRenderer render;
+    private void Start()
+    {
+        render  = GetComponent<SpriteRenderer>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // 충돌한 오브젝트가 공인지 확인
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            count++;
+            if (count ==1)
+            {   //원래 색상으로 변경
+                render.color = new Color(1f, 1f, 1f);
+            }
+            if (count == 2)
+            {
+                // 벽돌 제거
+                Destroy(gameObject);
+                GameManager.I.destroyBrickNum++;
+            }
+
+            if (GameManager.I.destroyBrickNum > 9)
+            {
+                if (GameManager.I.lv1 >= 5)
+                {
+                    GameManager.I.PassiveBtn1.interactable = false;
+                }
+                else if (GameManager.I.lv2 >= 5)
+                {
+                    GameManager.I.PassiveBtn2.interactable = false;
+                }
+                else if (GameManager.I.lv3 >= 5)
+                {
+                    GameManager.I.PassiveBtn3.interactable = false;
+                }
+                GameManager.I.passiveText1.text = "LV" + GameManager.I.lv1;
+                GameManager.I.passiveText2.text = "LV" + GameManager.I.lv2;
+                GameManager.I.passiveText3.text = "LV" + GameManager.I.lv3;
+                GameManager.I.IsLevelOver();
+                GameManager.I.getPassivePanel.SetActive(true);
+                Time.timeScale = 0f;
+
+            }
+
+        }
+
+    }
+
+}
