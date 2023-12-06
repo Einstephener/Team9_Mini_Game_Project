4b7926b4880cb5a11a1b54c9f21da12077c16929
WooJin <106531904+h117562@users.noreply.github.com>
2023-12-06 12:03:58 +0900

    fix - 공 이펙트 잔상 오류 해결

--- Assets/Scripts/GameOverWall.cs
+++ Assets/Scripts/GameOverWall.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class GameOverWall : MonoBehaviour
 {
-    
+    //오브젝트를 n초뒤에 지연 삭제
+    IEnumerator DelayDestroy(GameObject obj, float time)
+    {
+        yield return new WaitForSeconds(time); // n초 지연
+
+        Destroy(obj);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // 충돌한 오브젝트가 공인지 확인
         if (collision.gameObject.CompareTag("Ball"))
         {
-            Debug.Log("공 떨어짐");
-            Destroy(collision.gameObject);
+            StartCoroutine(DelayDestroy(collision.gameObject, 1));//1초뒤에 공 삭제
             GameManager.I.BallDead();
         }    
     }
